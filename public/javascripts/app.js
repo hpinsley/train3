6de805a73c156e026fca71e3da0be4e277a5b304
hpinsley <hpinsley@gmail.com>
2014-10-28 08:20:14 -0400

    Added angular routine

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,11 @@
-angular.module('train', []);
+angular.module('train', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeController'
+            })
+            .otherwise({
+               redirectTo: '/'
+            });
+    }]);
---
+++ public/javascripts/controllers/HomeController.js
@@ -0,0 +1,4 @@
+angular.module("train")
+    .controller("HomeController", function($scope){
+        $scope.title = "This is from angular home controller";
+    });
