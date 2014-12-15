6b12283a04b64a4d23188a810560f9bf7322aab8
hpinsley <hpinsley@gmail.com>
2014-12-15 09:18:25 -0500

    First attempt at an up time directive.

--- public/javascripts/directives/helperDirectives.js
+++ public/javascripts/directives/helperDirectives.js
@@ -16,4 +16,51 @@ angular.module('train')
 
             }
         };
+    })
+    .directive("hpTimeUp", function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            template: '<button ng-transclude></button>',
+            link: function(scope, iElement, iAttrs) {
+                var target = iAttrs.target;
+                var increment = parseInt(iAttrs.increment);
+                var targetCtl = $("#" + target);
+
+                iElement.click(function(){
+
+                    var timeVal = targetCtl.val();
+                    if (!timeVal) {
+                        targetCtl.val("09:00");
+                    }
+                    else {
+                        var parts = timeVal.split(":");
+                        var hour = parseInt(parts[0]);
+                        var minutes = parseInt(parts[1]);
+                        minutes += increment;
+                        if (minutes >= 60) {
+                            minutes -= 60;
+                            hour += 1;
+                            if (hour == 24) {
+                                hour = 0;
+                            }
+                        }
+                        var hourStr = hour.toString();
+                        if (hourStr.length === 1) {
+                            hourStr = "0" + hourStr;
+                        }
+
+                        var minStr = minutes.toString();
+                        if (minStr.length === 1) {
+                            minStr = "0" + minStr;
+                        }
+                        var newVal = hourStr + ":" + minStr;
+                        targetCtl.val(newVal);
+                        $scope.apply();
+                        targetCtl.focus();
+                    }
+                });
+            }
+        }
     });
