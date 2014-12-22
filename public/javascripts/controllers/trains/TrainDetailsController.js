d7b1a63bd07514c11b2220810b6e703e144a5171
hpinsley <hpinsley@gmail.com>
2014-12-22 09:19:16 -0500

    No longer call back to the server to display the station name after an add.  Rather use the filter cache.

--- public/javascripts/controllers/trains/TrainDetailsController.js
+++ public/javascripts/controllers/trains/TrainDetailsController.js
@@ -3,22 +3,12 @@ angular.module("train")
 
         $scope.afterAdd = false;
 
-        var displayTrainSummary = function() {
-            trainServices.getStation($scope.train.originStation).then(function(res){
-                $scope.originStation = res.data;
-            });
-            trainServices.getStation($scope.train.terminalStation).then(function(res){
-                $scope.terminalStation = res.data;
-            });
-        };
-
         var trainNumber = $routeParams["trainNumber"];
 
         $scope.trainNumber = trainNumber;
         trainServices.getTrain(trainNumber)
             .then(function(res){
                 $scope.train = res.data;
-                displayTrainSummary();
             });
 
         //for the select control
@@ -28,27 +18,23 @@ angular.module("train")
             });
 
         $scope.addStop = function(stopTime, station) {
-            //alert("Adding stop at " + stopTime + " for station " + station);
             trainServices.addStop($scope.trainNumber, stopTime, station)
                 .then(function(res){
                     return trainServices.getTrain($scope.trainNumber);
                 })
                 .then(function(res){
                     $scope.train = res.data;
-                    displayTrainSummary();
                     $scope.afterAdd = true;
                 });
         };
 
         $scope.deleteStop = function (trainNumber, stop) {
-            //alert("Deleting stop for train " + trainNumber + " at " + stop.time + " at " + stop.station);
             trainServices.deleteStop(trainNumber, stop)
                 .then(function (res) {
                     return trainServices.getTrain($scope.trainNumber);
                 })
                 .then(function (res) {
                     $scope.train = res.data;
-                    displayTrainSummary();
                 });
         }
     });
