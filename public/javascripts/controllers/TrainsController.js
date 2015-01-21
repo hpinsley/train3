f9f2c7e3e9690a694d59c75a2a17307099d4dad5
hpinsley <hpinsley@gmail.com>
2015-01-20 19:53:14 -0500

    Building fitlers for finding trains by start and stop station

--- public/javascripts/controllers/TrainsController.js
+++ public/javascripts/controllers/TrainsController.js
@@ -2,7 +2,10 @@ angular.module("train")
     .controller("TrainsController", function($scope, trainServices, $location){
         $scope.title = "This is from trains controller";
 
-        $scope.items = ["apple", "banana", "orange"]
+        trainServices.getStations()
+            .success(function(stations){
+                $scope.stations = stations;
+            });
 
         trainServices.getTrains()
             .then(function(res){
@@ -12,6 +15,24 @@ angular.module("train")
                     alert("Prop: " + prop + " = " + err[prop]);
                 }
             });
+
+        $scope.selectedStopsFn = function(train) {
+            var startStation = $scope.startStation;
+            var endStation = $scope.endStation;
+
+            if (!startStation || !endStation) {
+                return true;
+            }
+            var startIndex = _.findIndex(train.stops, function(stop) { return stop.station == startStation});
+            var endIndex = _.findIndex(train.stops, function(stop) { return stop.station == endStation});
+
+            return (startIndex >= 0 && endIndex >= 0 && endIndex > startIndex);
+        };
+
+        $scope.clearFilters = function() {
+            $scope.startStation = null;
+            $scope.endStation = null;
+        }
 
         $scope.newTrain = function() {
             trainServices.addTrain()
