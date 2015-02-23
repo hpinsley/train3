d39ebbe2b66f3250b8ad4592cd415e213b233213
hpinsley <hpinsley@gmail.com>
2015-02-23 09:21:00 -0500

    Fixed menu active setting

--- public/javascripts/controllers/bodyController.js
+++ public/javascripts/controllers/bodyController.js
@@ -1,4 +1,18 @@
 angular.module("train")
-    .controller("bodyController", function($scope){
+    .controller("bodyController", function($scope, $location){
         $scope.title = "This is the body controller title";
+
+        $scope.getClass = function(path) {
+            var curPath = $location.path();
+
+            if (path == "/") {
+                return (curPath == path) ? "active" : "";
+            }
+
+            if (curPath.substr(0, path.length) == path) {
+                return "active";
+            }
+
+            return "";
+        };
     });
