6ee54a01cd41c446b22687f31f149c9bb745871a
hpinsley <hpinsley@gmail.com>
2015-03-21 12:44:44 -0400

    Added tooltip support to the scatter plot.

--- public/javascripts/controllers/d3/TrainScatterController.js
+++ public/javascripts/controllers/d3/TrainScatterController.js
@@ -35,8 +35,8 @@ angular.module("train")
         }
 
         var plotStops = function(stops) {
-            var w = $("svg")[0].clientWidth;
-            var h = $("svg")[0].clientHeight;
+            var w = 1000;
+            var h = 500;
             var r = 5;
 
             var xMarginLeft = 100;
@@ -75,7 +75,22 @@ angular.module("train")
 //                console.log("Stop at " + stop.station + " at " +
 //                    stop.time + " maps to " + timeScale(helperServices.translateZuluString(stop.time)));
 //            });
-            var svg = d3.select("svg");
+
+
+            //var svg = d3.select("svg");
+
+            var tooltip = d3.select("div#container").append("div")
+                .attr("id", "tooltip")
+                .style("opacity", 0);
+
+            var svg = d3.select("div#container")
+                .append("svg")
+                .attr({
+                    width: w,
+                    height: h
+                });
+
+
             svg.selectAll('circle')
                 .data(stops)
                 .enter()
@@ -84,10 +99,21 @@ angular.module("train")
                     cx: function(d) { return MapStationToXValue(d.station);},
                     cy: function(d) { return MapTimeToYValue(d.time);},
                     r: r,
-                    fill: "red",
-                    title: function(d) { return "Stop at " + d.station;}
+                    fill: "red"
+                })
+                .on("mouseover", function(d){
+                    tooltip.transition()
+                        .duration(500)
+                        .style("opacity", .9);
+                    tooltip.html("<strong>Station: " + d.station + "</strong>")
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function(d) {
+                    tooltip.transition()
+                        .duration(500)
+                        .style("opacity", 0);
                 });
-
             var xAxisGen = d3.svg.axis().scale(stationScale)
                 .ticks(stationList.length)
                 .tickFormat(function(i){
@@ -120,73 +146,4 @@ angular.module("train")
                 .attr("class","axis")
                 .attr("transform", "translate(" + 80 + ",0)");
         };
-
-        var plotStationData = function(stats) {
-            var svg = d3.select("svg");
-            var width = $("svg")[0].clientWidth;
-            var height = $("svg")[0].clientHeight;
-            var stationCount = stats.length;
-            var xBarPadding = 50;
-            var yBarPaddingTop = 30;
-            var yBarPaddingBottom = 140;
-            var yAxisPadding = 40;
-            var barLabelPadding = 5;
-            var barWidth = 10;
-            var maxTrains = _.max(stats, function(stat){ return stat.trainCount; }).trainCount;
-            var yScale = d3.scale
-                .linear()
-                .domain([0,maxTrains])
-                .range([height-yBarPaddingBottom, yBarPaddingTop]);
-            var xScale = d3.scale
-                .linear()
-                .domain([0, stationCount-1])
-                .range([xBarPadding, width - xBarPadding]);
-
-            var yAxisGen = d3.svg.axis().scale(yScale).orient("left");
-            var xAxisGen = d3.svg.axis().scale(xScale)
-                .ticks(stats.length)
-                .tickFormat(function(i){
-                    return stats[i].station.name;
-                })
-                .orient("bottom");
-
-            var yAxis = svg.append("g").call(yAxisGen)
-                .attr("class","axis")
-                .attr("transform", "translate(" + yAxisPadding + ",0)");
-
-            var xAxis = svg.append("g").call(xAxisGen)
-                .attr("class","axis")
-                .attr("transform", "translate(0," + (height - yBarPaddingBottom) + ")");
-
-            xAxis.selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", ".15em")
-                .attr("transform", function(d) {
-                    return "rotate(-65)"
-                });
-
-            svg.selectAll('rect.bar')
-                .data(stats)
-                .enter()
-                .append('rect')
-                .attr({
-                    class: "bar",
-                    x: function(d,i) { return xScale(i);},
-                    y: function(d) { return yScale(d.trainCount);},
-                    width: function() { return barWidth;},
-                    height: function(d) { return height - yBarPaddingBottom - yScale(d.trainCount);}
-                });
-
-            svg.selectAll('text.barLabel')
-                .data(stats)
-                .enter()
-                .append('text')
-                .text(function(d) { return d.trainCount; })
-                .attr({
-                    class: "barLabel",
-                    x: function(d,i) { return xScale(i);},
-                    y: function(d) { return yScale(d.trainCount) - barLabelPadding;}
-                });
-        }
     });
