92afabb8de0ff9cd5cf5de4018d8b3c59ecab99b
hpinsley <hpinsley@gmail.com>
2015-03-24 22:16:06 -0400

    First successful attempt at drawing Westchester county.

--- public/javascripts/controllers/d3/StationMapController.js
+++ public/javascripts/controllers/d3/StationMapController.js
@@ -3,13 +3,15 @@ angular.module("train")
         $scope.title = "Station Map";
 
         trainServices.getLines()
-            .success(function(lines){
-                $scope.lines = lines;
+            .then(function(res){
+                $scope.lines = res.data;
                 $scope.selectedLine = $scope.lines[0];
-            });
-        trainServices.getStations()
-            .success(function(stations) {
-                $scope.stations = stations;
+            })
+            .then(function(){
+                trainServices.getStations()
+                    .success(function(stations) {
+                        $scope.stations = stations;
+                    });
             });
 
         $scope.stationInLineFn = function(station) {
@@ -18,24 +20,76 @@ angular.module("train")
             });
         }
 
+        function PlotStationStops(svg, projection) {
+            d3.csv("data/sales-by-city.csv", function (data) {
+
+                svg.selectAll("circle")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function (d) {
+                        if (!d) {
+                            console.log("no d");
+                        }
+                        var p = projection([d.lon, d.lat]);
+                        if (!p || p.length == 0) {
+                            console.log("bad projection");
+                            return -1;
+                        }
+                        return p[0];
+                    })
+                    .attr("cy", function (d) {
+                        var p = projection([d.lon, d.lat]);
+                        if (!p || p.length == 0) {
+                            console.log("bad projection");
+                            return -1;
+                        }
+                        return p[1];
+                    })
+                    .attr("r", function (d) {
+                        return Math.sqrt(parseInt(d.sales) * 0.00005);
+                    })
+                    .style("fill", "red");
+
+            });
+        }
+
         $scope.plotMap = function() {
             var w = 700;
             var h = 500;
 
-            //Define map projection
-            var projection = d3.geo.albersUsa()
-                .translate([w/2, h/2])
-                .scale([500]);
-
-            //Define path generator
-            var path = d3.geo.path()
-                .projection(projection);
-
-            //Create SVG element
-            var svg = d3.select("#svgContainer").append("svg").attr({width:w, height: h});
-
             //Load in GeoJSON data
-            d3.json("data/us.json", function(json) {
+            d3.json("data/westchester.json", function(json) {
+
+                var bounds = d3.geo.bounds(json.features[0]);
+                var minLng = bounds[0][0];
+                var minLat = bounds[0][1];
+                var maxLng = bounds[1][0];
+                var maxLat = bounds[1][1];
+                var padding = 30;
+
+                var lngScale = d3.scale
+                    .linear()
+                    .domain([minLng, maxLng])
+                    .range([padding,w-padding]);
+
+                var latScale = d3.scale
+                    .linear()
+                    .domain([minLat, maxLat])
+                    .range([h-padding,padding]);
+
+                var customProjection = function(lngLat) {
+                    var lng = lngLat[0];
+                    var lat = lngLat[1];
+                    return [lngScale(lng),latScale(lat)];
+                };
+
+                //Define path generator
+                var path = d3.geo.path()
+                    .projection(customProjection);
+
+                //Create SVG element
+                var svg = d3.select("#svgContainer").append("svg").attr({width:w, height: h});
 
                 //Bind data and create one path per GeoJSON feature
                 svg.selectAll("path")
@@ -43,40 +97,11 @@ angular.module("train")
                     .enter()
                     .append("path")
                     .attr("d", path)
-                    .attr("fill","#666666");
+                    .attr("fill","#666666")
+                    .attr("stroke", "black");
 
                 //Load in cities data
-                d3.csv("data/sales-by-city.csv", function(data) {
-
-                    svg.selectAll("circle")
-                        .data(data)
-                        .enter()
-                        .append("circle")
-                        .attr("cx", function(d) {
-                            if (!d) {
-                                console.log("no d");
-                            }
-                            var p = projection([d.lon, d.lat]);
-                            if (!p || p.length == 0) {
-                                console.log("bad projection");
-                                return -1;
-                            }
-                            return p[0];
-                        })
-                        .attr("cy", function(d) {
-                            var p = projection([d.lon, d.lat]);
-                            if (!p || p.length == 0) {
-                                console.log("bad projection");
-                                return -1;
-                            }
-                            return p[1];
-                        })
-                        .attr("r", function(d) {
-                            return Math.sqrt(parseInt(d.sales)* 0.00005);
-                        })
-                        .style("fill", "red");
-
-                });
+                //PlotStationStops(svg, projection);
             });
         };
 
