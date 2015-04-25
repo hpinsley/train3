c008d521cecbd04a51df548f58e37c962d898c57
hpinsley <hpinsley@gmail.com>
2015-04-25 19:52:55 -0400

    More menu stuff.

--- public/javascripts/controllers/bodyController.js
+++ public/javascripts/controllers/bodyController.js
@@ -1,18 +1,3 @@
 angular.module("train")
     .controller("bodyController", function($scope, $location){
-        $scope.title = "This is the body controller title";
-
-        $scope.getClass = function(path) {
-            var curPath = $location.path();
-
-            if (path == "/") {
-                return (curPath == path) ? "active" : "";
-            }
-
-            if (curPath.substr(0, path.length) == path) {
-                return "active";
-            }
-
-            return "";
-        };
     });
