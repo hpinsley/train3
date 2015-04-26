37e95fa6e0c5a08fc6fca45acff676c79a166db2
hpinsley <hpinsley@gmail.com>
2015-04-26 18:34:06 -0400

    Filter the stations by line to keep the table reasonable

--- public/javascripts/controllers/stations/StationGridController.js
+++ public/javascripts/controllers/stations/StationGridController.js
@@ -4,7 +4,24 @@
 angular.module("train").controller("StationGridController", function ($scope, $q, trainServices) {
     var stationsPromise = trainServices.getStations();
     var trainsPromise = trainServices.getTrains();
+    var linesPromise = trainServices.getLines();
+    $q.all([linesPromise, stationsPromise, trainsPromise]).then(function (resVector) {
+        $scope.lines = resVector[0].data;
+        $scope.stations = resVector[1].data;
+        $scope.trains = resVector[2].data;
+        buildGrid();
+    });
+    $scope.stationFilterFn = function (station) {
+        if (!$scope.selectedLine) {
+            return false;
+        }
+        return _.contains($scope.selectedLine.stations, station.abbr);
+    };
+    $scope.selectLine = function (line) {
+        $scope.selectedLine = line;
+    };
     function buildGrid() {
+        $scope.selectedLine = $scope.lines[0];
         $scope.stations.forEach(function (fromStation) {
             $scope.stations.forEach(function (toStation) {
                 getFromToStationInfo(fromStation, toStation);
@@ -15,10 +32,5 @@ angular.module("train").controller("StationGridController", function ($scope, $q
     function getFromToStationInfo(from, to) {
         console.log("From %s to %s", from.abbr, to.abbr);
     }
-    $q.all([stationsPromise, trainsPromise]).then(function (resVector) {
-        $scope.stations = _.sortBy(resVector[0].data, "abbr");
-        $scope.trains = resVector[1].data;
-        buildGrid();
-    });
 });
 //# sourceMappingURL=StationGridController.js.map
--- public/javascripts/controllers/stations/StationGridController.ts
+++ public/javascripts/controllers/stations/StationGridController.ts
@@ -4,17 +4,44 @@
 
 interface StationGridControllerScope extends ng.IScope {
 
+    lines: TrainDefs.Line[];
     trains: TrainDefs.Train[];
     stations: TrainDefs.Station[];
-
+    stationFilterFn(station:TrainDefs.Station):boolean;
+    selectLine(line:TrainDefs.Line):void
+    selectedLine: TrainDefs.Line;
 }
+
 angular.module("train")
     .controller("StationGridController", function ($scope:StationGridControllerScope, $q:ng.IQService, trainServices) {
 
         var stationsPromise = trainServices.getStations();
         var trainsPromise = trainServices.getTrains();
+        var linesPromise = trainServices.getLines();
+
+        $q.all([linesPromise, stationsPromise, trainsPromise])
+            .then(function(resVector:any[]) {
+                $scope.lines = resVector[0].data;
+                $scope.stations = resVector[1].data;
+                $scope.trains = resVector[2].data;
+
+                buildGrid();
+            }
+        );
+
+        $scope.stationFilterFn = function(station:TrainDefs.Station) : boolean {
+            if (!$scope.selectedLine) {
+                return false;
+            }
+            return _.contains($scope.selectedLine.stations, station.abbr);
+        }
+
+        $scope.selectLine = function(line:TrainDefs.Line) {
+            $scope.selectedLine = line;
+        }
 
         function buildGrid() : void {
+            $scope.selectedLine = $scope.lines[0];
             $scope.stations.forEach((fromStation) => {
                 $scope.stations.forEach((toStation) => {
                     getFromToStationInfo(fromStation, toStation);
@@ -25,15 +52,5 @@ angular.module("train")
         function getFromToStationInfo(from:TrainDefs.Station, to:TrainDefs.Station) {
             console.log("From %s to %s", from.abbr, to.abbr);
         }
-
-        $q.all([stationsPromise, trainsPromise])
-            .then(function(resVector:any[]) {
-                $scope.stations = <TrainDefs.Station[]> _.sortBy(resVector[0].data, "abbr");
-                $scope.trains = resVector[1].data;
-
-                buildGrid();
-            }
-        );
-
 
     });
