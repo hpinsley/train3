9fff707a2c97e9a05457e86257e3020ac8fe9c68
hpinsley <hpinsley@gmail.com>
2015-04-27 09:13:19 -0400

    Build grid counts, tooltips, and hyperlinks for the grid.

--- public/javascripts/controllers/stations/StationGridController.js
+++ public/javascripts/controllers/stations/StationGridController.js
@@ -1,7 +1,62 @@
 /// <reference path="../../../../d.ts/lodash.d.ts" />
 /// <reference path="../../../../d.ts/traindefs.d.ts" />
 /// <reference path="../../../../d.ts/angular.d.ts" />
+var FromToInfo = (function () {
+    function FromToInfo(fromStation, toStation) {
+        this.fromStation = fromStation;
+        this.toStation = toStation;
+        this.trains = [];
+    }
+    FromToInfo.prototype.addTrain = function (train) {
+        this.trains.push(train);
+    };
+    FromToInfo.prototype.trainCount = function () {
+        return this.trains.length;
+    };
+    FromToInfo.prototype.trainListDisplay = function () {
+        var display = _.map(this.trains, function (train) {
+            return train.description;
+        }).join("\n");
+        return display;
+    };
+    return FromToInfo;
+})();
+var Collector = (function () {
+    function Collector() {
+        this.db = {};
+    }
+    Collector.prototype.clearDb = function () {
+        this.db = {};
+    };
+    Collector.prototype.makeKey = function (fromStation, toStation) {
+        return fromStation.abbr + "-" + toStation.abbr;
+    };
+    Collector.prototype.addTrain = function (fromStation, toStation, train) {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo;
+        fromToInfo = this.db[key];
+        if (!fromToInfo) {
+            fromToInfo = new FromToInfo(fromStation, toStation);
+            this.db[key] = fromToInfo;
+        }
+        fromToInfo.addTrain(train);
+    };
+    Collector.prototype.trainCount = function (fromStation, toStation) {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo;
+        fromToInfo = this.db[key];
+        return (fromToInfo && fromToInfo.trainCount()) || 0;
+    };
+    Collector.prototype.trainListDisplay = function (fromStation, toStation) {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo;
+        fromToInfo = this.db[key];
+        return (fromToInfo && fromToInfo.trainListDisplay()) || "";
+    };
+    return Collector;
+})();
 angular.module("train").controller("StationGridController", function ($scope, $q, trainServices) {
+    $scope.collector = new Collector();
     var stationsPromise = trainServices.getStations();
     var trainsPromise = trainServices.getTrains();
     var linesPromise = trainServices.getLines();
@@ -9,6 +64,7 @@ angular.module("train").controller("StationGridController", function ($scope, $q
         $scope.lines = resVector[0].data;
         $scope.stations = resVector[1].data;
         $scope.trains = resVector[2].data;
+        $scope.selectedLine = $scope.lines[0];
         buildGrid();
     });
     $scope.mouseOver = function (fromStation, toStation) {
@@ -26,17 +82,22 @@ angular.module("train").controller("StationGridController", function ($scope, $q
         $scope.fromStation = null;
         $scope.toStation = null;
     };
-    function buildGrid() {
-        $scope.selectedLine = $scope.lines[0];
-        $scope.stations.forEach(function (fromStation) {
-            $scope.stations.forEach(function (toStation) {
-                getFromToStationInfo(fromStation, toStation);
-            });
+    function getStation(abbr) {
+        return _.find($scope.stations, function (station) {
+            return station.abbr === abbr;
         });
     }
-    ;
-    function getFromToStationInfo(from, to) {
-        console.log("From %s to %s", from.abbr, to.abbr);
+    function buildGrid() {
+        _.forEach($scope.trains, function (train) {
+            console.log("buildGrid starting train %d", train.number);
+            for (var i = 0; i < train.stops.length - 1; ++i) {
+                var fromStation = getStation(train.stops[i].station);
+                for (var j = i + 1; j < train.stops.length; ++j) {
+                    var toStation = getStation(train.stops[j].station);
+                    $scope.collector.addTrain(fromStation, toStation, train);
+                }
+            }
+        });
     }
 });
 //# sourceMappingURL=StationGridController.js.map
--- public/javascripts/controllers/stations/StationGridController.ts
+++ public/javascripts/controllers/stations/StationGridController.ts
@@ -2,6 +2,76 @@
 /// <reference path="../../../../d.ts/traindefs.d.ts" />
 /// <reference path="../../../../d.ts/angular.d.ts" />
 
+class FromToInfo {
+
+    constructor(fromStation:TrainDefs.Station, toStation:TrainDefs.Station) {
+        this.fromStation = fromStation;
+        this.toStation = toStation;
+        this.trains = [];
+    }
+
+    fromStation: TrainDefs.Station;
+    toStation: TrainDefs.Station;
+    trains: TrainDefs.Train[];
+
+    addTrain(train:TrainDefs.Train) : void {
+        this.trains.push(train);
+    }
+
+    trainCount() : number {
+        return this.trains.length;
+    }
+
+    trainListDisplay() : string {
+        var display = _.map(this.trains, (train:TrainDefs.Train) => {
+            return train.description;
+        }).join("\n");
+
+        return display;
+    }
+}
+
+class Collector {
+
+    db = {};
+
+    clearDb() : void {
+        this.db = {}
+    }
+
+    makeKey(fromStation:TrainDefs.Station, toStation:TrainDefs.Station) : string {
+        return fromStation.abbr + "-" + toStation.abbr;
+    }
+
+    addTrain(fromStation:TrainDefs.Station, toStation:TrainDefs.Station, train:TrainDefs.Train) {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo:FromToInfo;
+
+        fromToInfo = this.db[key];
+        if (!fromToInfo) {
+            fromToInfo = new FromToInfo(fromStation, toStation);
+            this.db[key] = fromToInfo;
+        }
+        fromToInfo.addTrain(train);
+    }
+
+    trainCount(fromStation:TrainDefs.Station, toStation:TrainDefs.Station) : number {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo:FromToInfo;
+
+        fromToInfo = this.db[key];
+        return (fromToInfo && fromToInfo.trainCount()) || 0;
+    }
+
+    trainListDisplay(fromStation:TrainDefs.Station, toStation:TrainDefs.Station) : string {
+        var key = this.makeKey(fromStation, toStation);
+        var fromToInfo:FromToInfo;
+
+        fromToInfo = this.db[key];
+        return (fromToInfo && fromToInfo.trainListDisplay()) || "";
+    }
+}
+
 interface StationGridControllerScope extends ng.IScope {
 
     lines: TrainDefs.Line[];
@@ -13,10 +83,13 @@ interface StationGridControllerScope extends ng.IScope {
     mouseOver(fromStation:TrainDefs.Station,toStation:TrainDefs.Station) :void;
     fromStation: TrainDefs.Station;
     toStation: TrainDefs.Station;
+    collector: Collector;
 }
 
 angular.module("train")
     .controller("StationGridController", function ($scope:StationGridControllerScope, $q:ng.IQService, trainServices) {
+
+        $scope.collector = new Collector();
 
         var stationsPromise = trainServices.getStations();
         var trainsPromise = trainServices.getTrains();
@@ -27,6 +100,7 @@ angular.module("train")
                 $scope.lines = resVector[0].data;
                 $scope.stations = resVector[1].data;
                 $scope.trains = resVector[2].data;
+                $scope.selectedLine = $scope.lines[0];
 
                 buildGrid();
             }
@@ -50,17 +124,22 @@ angular.module("train")
             $scope.toStation = null;
         }
 
-        function buildGrid() : void {
-            $scope.selectedLine = $scope.lines[0];
-            $scope.stations.forEach((fromStation) => {
-                $scope.stations.forEach((toStation) => {
-                    getFromToStationInfo(fromStation, toStation);
-                });
+        function getStation(abbr:string) : TrainDefs.Station {
+            return _.find($scope.stations, (station) => {
+                return station.abbr === abbr;
             });
-        };
-
-        function getFromToStationInfo(from:TrainDefs.Station, to:TrainDefs.Station) {
-            console.log("From %s to %s", from.abbr, to.abbr);
         }
 
+        function buildGrid() : void {
+            _.forEach($scope.trains, (train:TrainDefs.Train) => {
+                console.log("buildGrid starting train %d", train.number);
+                for (var i = 0; i<train.stops.length - 1; ++i) {
+                    var fromStation = getStation(train.stops[i].station);
+                    for (var j = i + 1; j < train.stops.length; ++j) {
+                        var toStation = getStation(train.stops[j].station);
+                        $scope.collector.addTrain(fromStation, toStation, train);
+                    }
+                }
+            });
+        }
     });
